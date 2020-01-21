227ff86f62b6291a087438f24454a0a5a9148c73
liuchuang05 <liuchuang05@meituan.com>
2020-01-21 15:21:29 +0800

    init

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/chainal/lccte/lib"
+	"net/http"
 )
 
 func main() {
-	fmt.Println(lib.SayHello())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "This is my http server")
+		if err != nil {
+			fmt.Println("write response error")
+		}
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("start listen on 8080 error")
+	}
 }
